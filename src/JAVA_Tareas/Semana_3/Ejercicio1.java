b226a4072eb70ddf6755738a34e56edfa9a059a1
KrevsX <elmrochino,yo@gmail.com>
2023-01-19 00:02:30 -0600

    Update NewOps

--- src/JAVA_Tareas/Semana_3/Ejercicio1.java
+++ src/JAVA_Tareas/Semana_3/Ejercicio1.java
@@ -15,13 +15,13 @@ package JAVA_Tareas.Semana_3;
 import java.util.Scanner;
 
 public class Ejercicio1 {
-    /*  1) Escriba un programa para calcular el salario de un empleado sabiendo que su
+/*  1) Escriba un programa para calcular el salario de un empleado sabiendo que su
         salario diario es de $10.00 y que el pago por hora extra es del 10% del salario
         diario. El programa deberá permitir ingresar el nombre del empleado, el número
         de días trabajados, el salario diario y el número total de horas extras. El
         programa deberá de imprimir el nombre del empleado y el salario final del
         empleado.
-    */
+*/
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
---
+++ src/JAVA_Tareas/Semana_3/Ejercicio2.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023. By.Kevin Alas
+ * All rights reserved
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia
+ * accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget
+ * metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package JAVA_Tareas.Semana_3;
+
+public class Ejercicio2 {
+/*
+    2) Una empresa de venta de computadoras paga a su personal de ventas un salario
+    de $300,00 más una comisión de $50,00 por computadora vendida. Diseñar un
+    programa para calcular el salario de un vendedor en un determinado mes,
+    conociendo el nº de computadoras vendidas. Al salario devengado se debe de
+    retener el 10% en concepto de renta. Imprimir el nombre del vendedor, el salario
+    devengado y la retención de la renta.
+*/
+
+    public static void main(String[] args) {
+
+    }
+}
